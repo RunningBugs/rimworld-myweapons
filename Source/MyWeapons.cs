23e56b918b297a32f0508678db6767fd57aa6899
Sanhu Li <lisanhu2014@hotmail.com>
2023-07-20 03:21:03 -0400

    Added super psychicshock auto turrets (with C#), added super shield belt, added recipe to gain chemfuel, added recipe to gain lavish meals, added translation and description text for super gun

---
+++ Source/MyWeapons.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MyWeapons
+{
+    [StaticConstructorOnStartup]
+    public static class LoadingScreen
+    {
+        static LoadingScreen()
+        {
+            Log.Message("My Weapons Initialized");
+        }
+    }
+}
