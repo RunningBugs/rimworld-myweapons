5c5141a961eaa917fea59db18003d372e95053e5
Sanhu Li <lisanhu2014@hotmail.com>
2023-07-24 07:53:46 -0400

    Update Alert to enalbe units selection

--- Source/TimedMailWindow.cs
+++ Source/TimedMailWindow.cs
@@ -2,16 +2,18 @@
 using Verse;
 using UnityEngine;
 using HarmonyLib;
+using System.Collections.Generic;
 
 namespace MyWeapons
 {
     public class TimedMailWindow : Window
     {
 
-        public override Vector2 InitialSize => new Vector2(600f, 350f);
+        public override Vector2 InitialSize => new Vector2(600f, 330f);
 
         private string buffer = "0";
         private string txtBuffer = "";
+        private int unit = 0;
 
         // Seems like starting x, y and width height
         protected override void SetInitialSizeAndPosition()
@@ -24,6 +26,50 @@ namespace MyWeapons
             base.PreOpen();
         }
 
+        private int radioButtonGroupHorizontal(Rect inRect, List<string> labels, int res)
+        {
+            int count = labels.Count;
+            float width = inRect.width / count;
+            List<Rect> rects = new List<Rect>();
+
+            for (int i = 0; i < count; ++i)
+            {
+                Rect rect = new Rect(inRect.x + width * i, inRect.y, width, inRect.height);
+                rects.Add(rect);
+            }
+
+            int num = res;
+            if (res >= 0 && res < count)
+            {
+                for (int i = 0; i < count; ++i)
+                {
+                    if (Widgets.RadioButtonLabeled(rects[i].LeftHalf().Rounded(), labels[i], i == res))
+                    {
+                        num = i;
+                    }
+                }
+            }
+
+            return num;
+        }
+
+        private int getMultiplier(int unit)
+        {
+            switch (unit)
+            {
+                case 0:
+                    return 1;
+                case 1:
+                    return GenDate.TicksPerHour;
+                case 2:
+                    return GenDate.TicksPerDay;
+                case 3:
+                    return GenDate.TicksPerYear;
+                default:
+                    return 1;
+            }
+        }
+
         public override void DoWindowContents(Rect inRect)
         {
             var listView = new Listing_Standard(GameFont.Small);
@@ -32,8 +78,19 @@ namespace MyWeapons
             listView.Label("SetTimer".Translate());
             listView.Gap();
 
-            Text.Anchor = TextAnchor.MiddleLeft;
-            int ticks = 0;
+            // Unit selection: ticks, hours, days, years
+            //  Could create a RadioButtonGroup function
+            List<string> labels = new List<string>();
+            labels.Add("ticks");
+            labels.Add("hours");
+            labels.Add("days");
+            labels.Add("years");
+            Log.Warning($"{unit}");
+            unit = radioButtonGroupHorizontal(listView.GetRect(Text.LineHeight), labels, unit);
+            Log.Warning($"{unit}");
+            listView.Gap();
+
+            int ticks = 0, real_ticks = 0;
             listView.TextFieldNumericLabeled<int>("TicksToAlert".Translate(), ref ticks, ref buffer);
             listView.Gap();
 
@@ -41,14 +98,16 @@ namespace MyWeapons
             listView.Gap();
 
             Text.Anchor = TextAnchor.MiddleLeft;
-            txtBuffer = listView.TextEntryLabeled("LetterMessage".Translate(), txtBuffer, 5);
+            txtBuffer = listView.TextEntryLabeled("LetterMessage".Translate(), txtBuffer, 2);
             listView.Gap();
 
             bool close = listView.ButtonText("Set".Translate());
             if (close)
             {
                 int ticksToAlert = Find.TickManager.TicksGame;
-                AlertUtility.Add(new AlertUtility.Event(ticksToAlert + ticks, txtBuffer));
+                int multiplier = getMultiplier(unit);
+                real_ticks = ticks * multiplier;
+                AlertUtility.Add(new AlertUtility.Event(ticksToAlert + real_ticks, txtBuffer));
                 Find.WindowStack.TryRemove(typeof(TimedMailWindow));
             }
 
