3d1fefa43b672ff1cd88e9e1c6980a41d8083b60
Sanhu Li <lisanhu2014@hotmail.com>
2023-12-08 05:32:41 -0500

    Adding caller filepath, line number, member name to the logging tool. Adding a tmp file for experimenting patches. Adding a commented class to check values.

--- Source/MyWeapons.cs
+++ Source/MyWeapons.cs
@@ -1,6 +1,9 @@
 ﻿using Verse;
 using HarmonyLib;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using RimWorld;
+using Verse.AI;
 
 
 namespace MyWeapons
@@ -17,12 +20,36 @@ namespace MyWeapons
         }
     }
 
+    
+
+    // public class Tracker : MapComponent
+    // {
+    //     public Tracker(Map map) : base(map)
+    //     {
+    //     }
+
+    //     public override void MapComponentTick()
+    //     {
+    //         base.MapComponentTick();
+    //         if (Find.TickManager.TicksGame % 100 == 0)
+    //         {
+    //             foreach (var thing in Find.CurrentMap.spawnedThings)
+    //             {
+    //                 if (thing is Pawn p && p.RaceProps.IsMechanoid)
+    //                 {
+    //                     Log.Warning(p.Name + $"{p.def}");
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
     public static class Log
     {
         const string prefix = "[MyWeapons] ";
-        public static void Warning(string msg)
+        public static void Warning(string msg, [CallerFilePath] string fileName = "", [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = 0)
         {
-            Verse.Log.Warning(prefix + msg);
+            Verse.Log.Warning(prefix + $"[ {fileName}:{lineNumber} {memberName} ]" + msg);
         }
     }
 }
