359a0c71e289b74d12ab9e604a83afa05b8a8279
Sanhu Li <lisanhu2014@hotmail.com>
2024-08-01 16:37:29 -0400

    Update the stun launcher to make it not harming explosive items, also make it not harming allies and our units

---
+++ 1.5/Source/DamageWorker_MyStun.cs
@@ -0,0 +1,47 @@
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Logs = MyWeapons.Log;
+
+namespace MyWeapons;
+
+[DefOf]
+public class MyDefOfs
+{
+    public static DamageDef MyStun;
+}
+
+class DamageWorker_MyStun : DamageWorker
+{
+    public override DamageWorker.DamageResult Apply(DamageInfo dinfo, Thing victim)
+    {
+        if (dinfo.Def == MyDefOfs.MyStun)
+        {
+            if (!victim.HostileTo(dinfo.Instigator))
+            {
+                return new DamageWorker.DamageResult();
+            }
+            var ticks = (int)(dinfo.Amount * 30f);
+            if (victim is Pawn pawn)
+            {
+                if (pawn.stances != null)
+                {
+                    pawn.stances.stunner.StunFor(ticks, dinfo.Instigator);
+                }
+            }
+            else if (victim is Building)
+            {
+                CompStunnable comp = victim.TryGetComp<CompStunnable>();
+                if (comp != null)
+                {
+                    comp.StunHandler.StunFor(ticks, dinfo.Instigator);
+                }
+            }
+            return new DamageWorker.DamageResult();
+        }
+        else
+        {
+            return base.Apply(dinfo, victim);
+        }
+    }
+}
--- 1.5/Source/MyWeapons.cs
+++ 1.5/Source/MyWeapons.cs
@@ -36,5 +36,15 @@ namespace MyWeapons
         {
             Verse.Log.Warning(prefix + $"[ {fileName}:{lineNumber} {memberName} ]" + msg);
         }
+
+        public static void Message(string msg, [CallerFilePath] string fileName = "", [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = 0)
+        {
+            Verse.Log.Message(prefix + $"[ {fileName}:{lineNumber} {memberName} ]" + msg);
+        }
+
+        public static void Error(string msg, [CallerFilePath] string fileName = "", [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = 0)
+        {
+            Verse.Log.Error(prefix + $"[ {fileName}:{lineNumber} {memberName} ]" + msg);
+        }
     }
 }
